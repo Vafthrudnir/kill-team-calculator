de2e4517d28064b37c3e3153b8ea98a9248aff86
Vafthrudnir <szd0517@gmail.com>
2023-05-21 02:26:21 +0200

    simulate endpoint implemented

--- kill-team-simulator.py
+++ kill-team-simulator.py
@@ -1,7 +1,7 @@
 import statistics
 import json
 import os
-from flask import Flask
+from flask import Flask, request
 
 import simulate_combat, test_values
 
@@ -20,17 +20,17 @@ weapon_defaults = {
 	'relentless': False
 }
 
+SAMPLES = 10000
 
-@app.route("/simulate_all")
-def simulate_all():
-	SAMPLES = 100000
 
+def combine_operatives(attacker_dict, defender_dict):
 	results = ''
-	for weapon_name, weapon in test_values.weapons.items():
+	for weapon_name, weapon in attacker_dict.items():
+		print(weapon_name)
 		for stat in weapon_defaults.keys():
 			if stat not in weapon.keys():
 				weapon[stat] = weapon_defaults[stat]
-		for defender_name, defender in test_values.defenders.items():
+		for defender_name, defender in defender_dict.items():
 			# simulate SAMPLE cases, average out results
 			damages = []
 			for i in range(0,SAMPLES):
@@ -39,17 +39,31 @@ def simulate_all():
 			deviation = statistics.stdev(damages)
 			print(f'{weapon_name} - {defender_name}: {average_damage:.2f} - {deviation:.2f}')
 			results += f'{weapon_name} - {defender_name}: {average_damage:.2f} - {deviation:.2f}\n'
+	return results
+
+
+@app.route('/simulate_all')
+def simulate_all():
+	results = combine_operatives(test_values.weapons, test_values.defenders)
 	return '<pre>' + results + '</pre>'
 
 
-@app.route("/teams/<team>")
+@app.route('/teams/<team>')
 def get_team(team):
-	with open(f'datasets/{team}.json') as team_handler:
-		team_dict = json.load(team_handler)
+	team_details = load_team(team)
+	return team_details if team_details else (f'team "{team}" does not exist!', 400)
+
+
+def load_team(team):
+	try:
+		with open(f'datasets/{team}.json') as team_handler:
+			team_dict = json.load(team_handler)
+	except OSError:
+			return None
 	return team_dict
 
 
-@app.route("/teams/")
+@app.route('/teams/')
 def get_team_list():
 	response = '<pre>'
 	res = []
@@ -58,3 +72,37 @@ def get_team_list():
 			res.append(os.path.splitext(path)[0])
 	print(res)
 	return res
+
+
+@app.route('/simulate')
+def simulate():
+	content = request.json
+	attacker = load_team(content['attacker'])
+	defender = load_team(content['defender'])
+	if attacker == None:
+		return f'team {content["attacker"]} does not exist!', 400
+	elif defender == None:
+		return f'team "{content["defender"]}" does not exist!', 400
+
+	attacker_dict = {}
+	for attacker_description in attacker.values():
+		for operative, op_details in attacker_description['operatives'].items():
+			try:
+				for weapon_name, weapon in op_details['ranged-weapons'].items():
+					attacker_dict[f'{operative} - {weapon_name}'] = weapon
+			except KeyError:
+				pass
+		for equipment, eq_details in attacker_description['equipment'].items():
+			try:
+				if eq_details['stand-alone']:
+					attacker_dict[f'{equipment}'] = eq_details['stats']
+			except KeyError:
+				pass
+
+	defender_dict = {}
+	for defender_description in defender.values():
+		for operative, op_details in defender_description['operatives'].items():
+			defender_dict[operative] = op_details['stats']
+
+	results = combine_operatives(attacker_dict, defender_dict)
+	return '<pre>' + results + '</pre>'
